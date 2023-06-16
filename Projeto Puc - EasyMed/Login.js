99ad594135fae41e493b1f8bacd1e9cb2fa53f09
Bruno Alves da Fonseca <bruno.fonseca.1018136@sga.pucminas.br>
2023-06-16 13:36:18 -0300

    Login/Registro finalizados

--- Projeto Puc - EasyMed/Login.js
+++ Projeto Puc - EasyMed/Login.js
@@ -20,15 +20,114 @@ iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
 
+// BOTÕES DE OCULTAR E MOSTRAR SENHA
+let btn = document.querySelector('.iconeye')
 
-// Cadastro do usuario e criacao da conta - EM PROGRESSO 
-const emailInput = loginForm.querySelector('input[type="email"]');
-    const passwordInput = loginForm.querySelector('input[type="password"]');
-    const emailValue = emailInput.value;
-    const passwordValue = passwordInput.value;
-const loginData = {
-      email: emailValue,
-      password: passwordValue
+btn.addEventListener('click', ()=>{
+    let inputSenha = document.querySelector('#loginPassword')
+
+    if(inputSenha.getAttribute('type') === 'password'){
+    inputSenha.setAttribute('type', 'text')}
+    else {
+        inputSenha.setAttribute('type', 'password')}
+    
+})
+
+let btn2 = document.querySelector('.iconeye2')
+
+btn2.addEventListener('click', ()=>{
+    let inputSenha = document.querySelector('#password')
+
+    if(inputSenha.getAttribute('type') === 'password'){
+    inputSenha.setAttribute('type', 'text')}
+    else {
+        inputSenha.setAttribute('type', 'password')}
+    
+})
+
+// LOGIN E REGISTRO DE USUÁRIOS 
+let users = [];
+const usersJSON = localStorage.getItem('users');
+if (usersJSON) {
+    users = JSON.parse(usersJSON);
+}
+
+const registerForm = document.getElementById('registerForm');
+registerForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const userExists = users.some(function(user) {
+        return user.username === username;
+    });
+
+    if (userExists) {
+        showMessage('Esse e-mail já foi cadastrado.');
+        return;
+    }
+
+    const newUser = {
+        username: username,
+        email: email,
+        password: password
     };
-const jsonData = JSON.stringify(loginData);
-const loginForm = document.querySelector('.form-box.login form');
+    users.push(newUser);
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    showMessage('Registro concluído.');
+    registerForm.reset();
+});
+
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const user = users.find(function(user) {
+        return user.email === email && user.password === password;
+    });
+
+    if (user) {
+        showMessage('Bem vindo, ' + user.username + '!');
+        loginForm.reset();
+    } else {
+        showMessage('Email ou senha inválidos.');
+    }
+});
+
+function showMessage(message) {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = message;
+}
+
+// BOTÃO LEMBRAR A SENHA
+
+const passwordInput = document.getElementById('loginPassword');
+const rememberCheckbox = document.getElementById('remember-forgot');
+const loginbtn = document.getElementById('loginbtn');
+
+const storedPassword = localStorage.getItem('rememberedPassword');
+if (storedPassword) {
+  passwordInput.value = storedPassword;
+  rememberCheckbox.checked = true;
+}
+
+loginbtn.addEventListener('click', function() {
+  const password = passwordInput.value;
+
+  if (rememberCheckbox.checked) {
+    localStorage.setItem('rememberedPassword', password);
+  } else {
+    localStorage.removeItem('rememberedPassword');
+  }
+});
+window.addEventListener('load', function() {
+    if (rememberCheckbox.checked && passwordInput.value) {
+      passwordInput.value = localStorage.getItem('rememberedPassword');
+    }
+});
